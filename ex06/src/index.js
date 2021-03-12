c873ec601206ec9f2c2f4bd2c8f0ea4edd00ea63
Boris Karačić <boris.karacic@outlook.com>
2021-03-12 11:35:22 +0100

    Added index.js file - mad lib style variable creation

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    //Only change code below this line
+    
+    var myName = "Boris";
+    var myString = "My name is " + myName + " and I am well!";
+
+    //Only change code above this line
+
+    return {
+        myString,
+    };
+}
+
+console.log(main());
+module.exports = main;
