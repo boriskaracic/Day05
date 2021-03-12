9880da049cd1a77e035cd2cd779f9b338427f98f
Boris Karačić <boris.karacic@outlook.com>
2021-03-12 15:38:41 +0100

    Added index.js file - wordBlanks game

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+    
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdvert = "quickly";
+
+    var wordBlanks = "My " + myNoun + " was " + myAdjective + " and " + myVerb + " " + myAdvert + ".";
+
+    //Only change code above this line
+
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
